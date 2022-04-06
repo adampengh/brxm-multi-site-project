4e4d8a0cda68501c26f8cf7a2106867d6c8c8207
Adam Pengh <adam.pengh@bloomreach.com>
2022-04-06 09:49:56 -0700

    update redirect server config

--- frontend/react/server.js
+++ frontend/react/server.js
@@ -14,6 +14,7 @@
  * limitations under the License.
  */
 
+const axios = require('axios');
 const dotenv = require('dotenv');
 const express = require('express');
 const fs = require('fs');
@@ -22,10 +23,10 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const dotenvPath = path.resolve(process.cwd(), '.env');
 if (fs.existsSync(dotenvPath)) {
-  const { error } = dotenv.config({ path: dotenvPath });
-  if (error) {
-    throw error;
-  }
+    const { error } = dotenv.config({ path: dotenvPath });
+    if (error) {
+        throw error;
+    }
 }
 
 const PORT = process.env.PORT || 3000;
@@ -34,27 +35,60 @@ app.use(express.static(path.join(__dirname, 'build')));
 
 // Proxy robots.txt to brXM
 app.get('/robots.txt', createProxyMiddleware({
-  target: 'http://localhost:8080', // Only the protocol and domain
-  changeOrigin: true,
-  pathRewrite: {
-    '/robots.txt': '/site/robots.txt'
-  }
+    target: process.env.REACT_APP_CMS_BASE_URL, // Only the protocol and domain
+    changeOrigin: true,
+    pathRewrite: {
+        '/robots.txt': '/site/robots.txt'
+    }
 }));
 
 // Proxy sitemap.xml to brXM
 app.get('/sitemap.xml', createProxyMiddleware({
-  target: 'http://localhost:8080', // Only the protocol and domain
-  changeOrigin: true,
-  pathRewrite: {
-    '/sitemap.xml': '/site/sitemap.xml'
-  }
+    target: process.env.REACT_APP_CMS_BASE_URL, // Only the protocol and domain
+    changeOrigin: true,
+    pathRewrite: {
+        '/sitemap.xml': '/site/sitemap.xml'
+    }
 }));
 
+axios({
+    method: 'get',
+    url: `${process.env.REACT_APP_CMS_BASE_URL}/site/api/documents`,
+    timeout: 5000
+})
+    .then((response) => {
+        // handle success
+        const redirects = response.data._embedded.documents;
+        redirects.forEach((redirect, index) => {
+            if (redirect.rulefrom && redirect.ruleto) {
+                app.get(redirect.rulefrom, (req, res) => {
+                    let statusCode = 302;
+                    if (redirect.ruletype && redirect.ruletype === 'permanent-redirect') {
+                        statusCode = 301;
+                    }
+                    res.redirect(statusCode, redirect.ruleto);
+                })
+            }
+        })
 
-app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
+        app.get('/*', (req, res) => {
+            res.sendFile(path.join(__dirname, 'build', 'index.html'));
+        });
 
-app.listen(PORT, () => {
-  console.log(`Server listening on ${PORT}`);
-});
+        app.listen(PORT, () => {
+            console.log(`Server listening on ${PORT}`);
+        });
+    })
+    .catch((error) => {
+        // handle error
+        error = error.toJSON();
+        console.log(error.name, error.message);
+
+        app.get('/*', (req, res) => {
+            res.sendFile(path.join(__dirname, 'build', 'index.html'));
+        });
+
+        app.listen(PORT, () => {
+            console.log(`Server listening on ${PORT}`);
+        });
+    });
